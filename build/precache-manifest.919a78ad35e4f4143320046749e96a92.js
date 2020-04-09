d2ea15389821250cc9b8cd13e03b13dcc4b1ffb4
AcroLost <49655180+AcroLost@users.noreply.github.com>
2020-04-09 23:02:52 +0500

    Add files via upload

---
+++ build/precache-manifest.919a78ad35e4f4143320046749e96a92.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82dfffc920552374c17069413c1ac2b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d89f2d9b4178cceb4556",
+    "url": "/static/css/main.dff882d2.chunk.css"
+  },
+  {
+    "revision": "99f10fc3293e58098ec7",
+    "url": "/static/js/2.b9a1e3f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b9a1e3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d89f2d9b4178cceb4556",
+    "url": "/static/js/main.25028975.chunk.js"
+  },
+  {
+    "revision": "3f0fb7ee7c8ec8e75554",
+    "url": "/static/js/runtime-main.1d1ec0b5.js"
+  },
+  {
+    "revision": "bf80c094626ee44af80267dfde4ff420",
+    "url": "/static/media/boy.bf80c094.mp4"
+  },
+  {
+    "revision": "c495286fdc3bd004dce13cf5aba929c7",
+    "url": "/static/media/cat.c495286f.svg"
+  },
+  {
+    "revision": "85663da27db94bfed856b3746d83329d",
+    "url": "/static/media/dog.85663da2.svg"
+  },
+  {
+    "revision": "4c857c2a813232bff9cc04c681c88373",
+    "url": "/static/media/fav_inactive.4c857c2a.png"
+  },
+  {
+    "revision": "ec0401f8a7d9c8f94105e71869f58a63",
+    "url": "/static/media/fox.ec0401f8.svg"
+  },
+  {
+    "revision": "5cced49d14a02f93db5e1cb90bbea6d1",
+    "url": "/static/media/koala.5cced49d.svg"
+  },
+  {
+    "revision": "3c6574b3d824f91dc49e4c2e94a95740",
+    "url": "/static/media/lion.3c6574b3.svg"
+  },
+  {
+    "revision": "1d9a953741616937b0bb583f57f0a941",
+    "url": "/static/media/owl.1d9a9537.svg"
+  },
+  {
+    "revision": "8e2badb79083647b76740e0f3504915a",
+    "url": "/static/media/penguin.8e2badb7.svg"
+  },
+  {
+    "revision": "296123ee21f7e15e782f0f100100f51e",
+    "url": "/static/media/pig.296123ee.svg"
+  },
+  {
+    "revision": "7c68f01420eaf9261115bfb76591d3a1",
+    "url": "/static/media/raccoon.7c68f014.svg"
+  },
+  {
+    "revision": "11a8f47e01fe816b8ef48fd9fd3bb810",
+    "url": "/static/media/react.11a8f47e.svg"
+  },
+  {
+    "revision": "2e62bc7b325fee3c5ca65a1d0e1c8c2a",
+    "url": "/static/media/sheep.2e62bc7b.svg"
+  },
+  {
+    "revision": "93fec09c9929ab21a395f2fd81b8b041",
+    "url": "/static/media/shoe.93fec09c.mp4"
+  }
+]);
